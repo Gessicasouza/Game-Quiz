660558bf28694818d136548cdaaa6ae6a1a01697
Milena Figueredo <milena.silva@zup.com.br>
2019-09-20 14:46:55 -0300

    adicionar metodos de pergunta certa e id certo

---
+++ src/main/java/br/com/game/models/User.java
@@ -0,0 +1,73 @@
+package br.com.game.models;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private Long id;
+	
+	private String name;
+	
+	private String email;
+	
+	private boolean administrator;
+	
+	@ElementCollection
+	private List<Long> answeredQuestions ;
+	
+	public User () {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isAdministrator() {
+		return administrator;
+	}
+
+	public void setAdministrator(boolean administrator) {
+		this.administrator = administrator;
+	}
+
+	public List<Long> getAnsweredQuestions() {
+		return answeredQuestions;
+	}
+
+	public void setAnsweredQuestions(List<Long> answeredQuestions) {
+		this.answeredQuestions = answeredQuestions;
+	}
+	
+}
